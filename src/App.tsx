a7c423aa11d410a0e3e82f13f6172a8f5f7f7758
Sun Jang <realjangsun@gmail.com>
2019-05-31 15:19:23 +0900

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './Routes';
+import { Normalize } from 'styled-normalize';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Routes />
+      <Normalize />
     </div>
   );
 };
